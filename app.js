14ddd1b69942ea05707186869e48de6a04f76273
omarGIT123 <103640522+omarGIT123@users.noreply.github.com>
2024-12-10 15:26:22 +0100

    adjust

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const indexRoutes = require("./routes/index");
-const https = require('https');
-const fs = require('fs');
-const http = require('http');
+const https = require("https");
+const fs = require("fs");
+const http = require("http");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 443; // Set port from .env or fallback to 443 for HTTPS
 
 const options = {
-  cert: fs.readFileSync('../server.crt'),  // Replace with actual path
-  key: fs.readFileSync('../server.key')    // Replace with actual path
+  cert: fs.readFileSync("../server.crt"), // Replace with actual path
+  key: fs.readFileSync("../server.key"), // Replace with actual path
 };
 
 // Middleware global
@@ -19,8 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
-  "https://omar-bradai-portfolio.onrender.com",  // First allowed origin (no trailing slash)
-  "https://omar-bradai-portfolio.tn"   // Second allowed origin (no trailing slash)
+  "https://omar-bradai-portfolio.onrender.com", // First allowed origin (no trailing slash)
+  "https://omar-bradai-portfolio.tn", // Second allowed origin (no trailing slash)
 ];
 
 app.use(
@@ -36,7 +36,7 @@ app.use(
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );
 
@@ -51,9 +51,13 @@ server.listen(PORT, () => {
 });
 
 // Optional: Redirect HTTP traffic to HTTPS
-http.createServer((req, res) => {
-  res.writeHead(301, { "Location": "https://" + req.headers["host"] + req.url });
-  res.end();
-}).listen(80, () => {
-  console.log('HTTP server running on port 80, redirecting to HTTPS');
-});
+http
+  .createServer((req, res) => {
+    res.writeHead(301, {
+      Location: "https://" + req.headers["host"] + req.url,
+    });
+    res.end();
+  })
+  .listen(80, () => {
+    console.log("HTTP server running on port 80, redirecting to HTTPS");
+  });
