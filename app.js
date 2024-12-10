a47a150d19d83968bc5007c1853049631be65873
omarGIT123 <103640522+omarGIT123@users.noreply.github.com>
2024-12-10 15:59:24 +0100

    fixed app.js

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const fs = require("fs");
 const http = require("http");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 443; // Set port from .env or fallback to 443 for HTTPS
+const PORT = process.env.PORT || 443;
 
 const options = {
-  cert: fs.readFileSync("../server.crt"), // Replace with actual path
-  key: fs.readFileSync("../server.key"), // Replace with actual path
+  key: fs.readFileSync("/etc/letsencrypt/live/api.omarbradai.tn/privkey.pem"),
+  cert: fs.readFileSync(
+    "/etc/letsencrypt/live/api.omarbradai.tn/fullchain.pem"
+  ),
 };
 
 // Middleware global
@@ -19,38 +21,33 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
-  "https://omar-bradai-portfolio.onrender.com", // First allowed origin (no trailing slash)
-  "https://omar-bradai-portfolio.tn", // Second allowed origin (no trailing slash)
+  "https://omar-bradai-portfolio.onrender.com",
+  "https://portfolio.omarbradai.tn",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Check if the origin is in the allowedOrigins list
       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        // Allow the request
         callback(null, true);
       } else {
-        // Deny the request
         callback(new Error("Not allowed by CORS"));
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    credentials: true,
   })
 );
 
 // Routes
 app.use("/", indexRoutes);
 
-// Start HTTPS server on port 443
-const server = https.createServer(sslOptions, app);
-server.timeout = 0; // Disable timeout
+const server = https.createServer(options, app);
+server.timeout = 0;
 server.listen(PORT, () => {
   console.log(`HTTPS server running on port ${PORT}`);
 });
 
-// Optional: Redirect HTTP traffic to HTTPS
 http
   .createServer((req, res) => {
     res.writeHead(301, {
